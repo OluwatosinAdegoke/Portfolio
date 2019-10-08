76d4be8b9a539cd2f62cf9a1f3d1b50fc930cb69
Oluwatosin Adegoke <o.p.adegoke@gmail.com>
2019-10-08 09:57:09 +0100

    Styling the video page

---
+++ src/resources/js/models/videos.js
@@ -0,0 +1,8 @@
+/*--------------------------------------DATA 4 VIDEOS-PAGE--------------------------------------*/
+import a from '../content/videos/mandem_feminism'
+import b from '../content/videos/taxman';
+import c from '../content/videos/hypeman_mock_1';
+
+let videos = [];
+videos.push(a, b, c);
+export default videos
