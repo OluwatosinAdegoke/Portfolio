c3673a2cfeed9e62e3be88548786dfb9dd6291ae
OluwatosinAdegoke <41165921+OluwatosinAdegoke@users.noreply.github.com>
2019-11-16 18:54:45 +0000

    Adding Videos #1
    - added new Taxman video
    - added new hypeman video
    - added talking tongues video

---
+++ src/resources/js/content/videos/talking_tongues_001.js
@@ -0,0 +1,8 @@
+let content = {
+    title: 'Taking Tongues Pilot',
+    description: 'Talking Tongues - The Talk Show all about Trainers! \nMeet our hosts and follow them on Instagram: Maya - @mayaaa_e \nTosin - @straight_juicing \nTee - @teeabraha \nStyling done by @Ashleighstunna, check out her website: http://ashleighstunna.com/ \nWe had a very special guest, Introducing the best Living Room host on Youtube Lippy, from Lippy\'s Living Room.  \n\nCatch his Youtube show on: https://www.youtube.com/watch?v=VLcIP...',
+    commentary: 'Thanks to our partner Urban MBA helping train these young creatives to create Talking Tongues. \nCheck out their website and support them! www.urbanmba.co.uk',
+    iframe: 'https://www.youtube.com/watch?v=_4jbjjy_FoE&t=209s'
+}
+
+export default content
--- src/resources/js/models/videos.js
+++ src/resources/js/models/videos.js
@@ -2,7 +2,8 @@
 import a from '../content/videos/mandem_feminism'
 import b from '../content/videos/taxman';
 import c from '../content/videos/hypeman_mock_1';
+import d from '../content/videos/talking_tongues_001'
 
 let videos = [];
-videos.push(a, b, c);
+videos.push(a, b, c, d);
 export default videos
